2e795b7722b1ab60cb10a38e17ed1cc138128ee0
Daniel Klein <othercriteria@gmail.com>
2015-02-12 13:17:40 -0500

    Count connections by day

--- analytics.py
+++ analytics.py
@@ -2,6 +2,7 @@
 
 import re
 import os
+import time
 from collections import defaultdict
 
 ipre = re.compile('[0-9]+\.[0-9]+\.[0-9]+\.[0-9]')
@@ -9,16 +10,24 @@ ipre = re.compile('[0-9]+\.[0-9]+\.[0-9]+\.[0-9]')
 log_files = os.listdir('logs/')
 
 ip_hits = defaultdict(int)
+date_hits = defaultdict(int)
 
 for lf in log_files:
     with file(os.path.join('logs',lf)) as l:
         first_in_file = True
         for line in l.readlines():
             p = line.split(' - ')
-            maybe_ip = p[0].split(' ')[-1]
+            ps = p[0].split(' ')
+
+            t = time.strptime((ps[2] + ps[3])[1:-1], '%d/%b/%Y:%H:%M:%S+0000')
+            maybe_ip = ps[-1]
+
             if ipre.match(maybe_ip):
                 if first_in_file:
                     ip_hits[maybe_ip] += 1
+                    date_hits[time.strftime("%Y-%m-%d", t)] += 1
+                    
                     first_in_file = False
 
 print sorted([(ip_hits[k],k) for k in ip_hits], reverse = True)
+print sorted([(k, date_hits[k]) for k in date_hits])
