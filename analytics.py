0ceb078026a8ac6ef9153c97eb8dae79e59f1ae3
Daniel Klein <othercriteria@gmail.com>
2015-02-20 14:26:37 -0500

    Report on names of resources accessed

--- analytics.py
+++ analytics.py
@@ -11,16 +11,21 @@ log_files = os.listdir('logs/')
 
 ip_hits = defaultdict(int)
 date_hits = defaultdict(int)
+res_hits = defaultdict(int)
 
 for lf in log_files:
     with file(os.path.join('logs',lf)) as l:
         first_in_file = True
         for line in l.readlines():
             p = line.split(' - ')
-            ps = p[0].split(' ')
 
-            t = time.strptime((ps[2] + ps[3])[1:-1], '%d/%b/%Y:%H:%M:%S+0000')
-            maybe_ip = ps[-1]
+            p0 = p[0].split(' ')
+            p1 = p[1].split(' ')
+
+            t = time.strptime((p0[2] + p0[3])[1:-1], '%d/%b/%Y:%H:%M:%S+0000')
+            maybe_ip = p0[-1]
+            if len(p1) >= 3 and not p1[0] in ['-', '"GET', '"PUT']:
+                res_hits[p1[2]] += 1
 
             if ipre.match(maybe_ip):
                 if first_in_file:
@@ -31,3 +36,4 @@ for lf in log_files:
 
 print sorted([(ip_hits[k],k) for k in ip_hits], reverse = True)
 print sorted([(k, date_hits[k]) for k in date_hits])
+print sorted([(res_hits[k],k) for k in res_hits], reverse = True)
