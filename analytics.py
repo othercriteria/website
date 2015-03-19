d8bede2c34598fa02b622b7b1ed3fac1014a2fd1
Daniel Klein <othercriteria@gmail.com>
2015-03-18 23:42:19 -0400

    Cache GeoIP results

--- analytics.py
+++ analytics.py
@@ -25,6 +25,7 @@ log_files = os.listdir('logs/')
 
 ip_hits = Counter()
 prefix_hits = defaultdict(set)
+country_hits = Counter()
 region_hits = Counter()
 date_hits = Counter()
 hour_hits = Counter()
@@ -34,20 +35,31 @@ class GeoIP():
     def __init__(self):
         self.conn = http.client.HTTPConnection('freegeoip.net')
 
+        try:
+            with open('cache/geoip.json', 'r') as infile:
+                self.cache = json.load(infile)
+        except:
+            self.cache = {}
+
     def fetch(self, ip):
+        if ip in self.cache:
+            return self.cache[ip]
+        
         self.conn.request('GET', '/json/' + ip)
 
         response = self.conn.getresponse()
-        print('GeoIP:', response.status, response.reason)
+        print('Querying GeoIP:', response.status, response.reason)
 
-        return response.read()
-
-    def report(self, ip):
-        data_serialized = self.fetch(ip).decode('utf-8')
-
+        data_serialized = response.read().decode('utf-8')
         data = json.loads(data_serialized)
 
+        self.cache[ip] = data
+        
         return data
+
+    def dump(self):
+        with open('cache/geoip.json', 'w') as outfile:
+            json.dump(self.cache, outfile)
 
 geoip = GeoIP()
 
@@ -84,8 +96,12 @@ for lf in log_files:
                 if first_in_file:
                     ip_hits[maybe_ip_loc.group()] += 1
 
-                    report = geoip.report(maybe_ip_loc.group())
+                    report = geoip.fetch(maybe_ip_loc.group())
 
+                    if 'country_name' in report:
+                        country_name = report['country_name']
+                        if not country_name == '':
+                            country_hits[report['country_name']] += 1
                     if 'region_name' in report:
                         region_name = report['region_name']
                         if not region_name == '':
@@ -101,11 +117,15 @@ for lf in log_files:
                     
                     first_in_file = False
 
+# Cache GeoIP data
+geoip.dump()
+                    
 print('IPs:', ip_hits.most_common())
 print('IP prefixes:', sorted([(k, prefix_hits[k]) for k in prefix_hits
                               if len(prefix_hits[k]) > 1],
                              key = lambda p: len(p[1]),
                              reverse = True))
+print('Countries:', country_hits.most_common())
 print('Regions:', region_hits.most_common())
 print('Dates:', sorted([(k, date_hits[k]) for k in date_hits]))
 print('Hours:', sorted([(k, hour_hits[k]) for k in hour_hits]))
