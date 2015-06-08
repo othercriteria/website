7ec42631689c923ec4ae6fa9d7ef4374c0fb7b76
Daniel Klein <othercriteria@gmail.com>
2015-06-07 23:07:18 -0400

    Super-crude storage of log files in "database"; still need to wipe old ones

--- analytics.py
+++ analytics.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import csv
+from io import StringIO
 import re
 import os
 from datetime import datetime, timedelta
@@ -30,7 +31,7 @@ def suspicious_agent(agent):
             'Google favicon' in agent or 'Disqus' in agent or
             'prismatic' in agent)
 
-log_files = os.listdir('logs/')
+new_log_files = os.listdir('logs/')
 
 ip_hits = Counter()
 ip_hits_all = Counter()
@@ -44,6 +45,23 @@ referrer_hits = Counter()
 agent_hits = Counter()
 possible_robots = set()
 excluded = set()
+
+class LogDB():
+    def __init__(self):
+        try:
+            with open('cache/logs.json', 'r') as infile:
+                self.db = json.load(infile)
+        except:
+            self.db = []
+
+    def add_new(self):
+        for infile in new_log_files:
+            with open(os.path.join('logs', infile), 'r') as log:
+                self.db.append(log.read())
+
+    def dump(self):
+        with open('cache/logs.json', 'w') as outfile:
+            json.dump(self.db, outfile)
 
 class GeoIP():
     def __init__(self):
@@ -78,92 +96,98 @@ class GeoIP():
         with open('cache/geoip.json', 'w') as outfile:
             json.dump(self.cache, outfile)
 
+# Update logs "database"
+logs = LogDB()
+logs.add_new()
+logs.dump()
+
+# Initialize GeoIP "database"
 geoip = GeoIP()
 
 # First pass to detect non-human access
-for infile in log_files:
-    with open(os.path.join('logs', infile), 'r') as log:
-        log_reader = csv.reader(log, delimiter = ' ', quotechar = '"')
+for log in logs.db:
+    s = StringIO(log)
+    log_reader = csv.reader(s, delimiter = ' ', quotechar = '"')
 
-        for line in log_reader:
-            remote_ip_str = line[4]
-            ip_hits_all[remote_ip_str] += 1
+    for line in log_reader:
+        remote_ip_str = line[4]
+        ip_hits_all[remote_ip_str] += 1
 
-            if suspicious_key(line[8]):
-                possible_robots.add(remote_ip_str)
-            if suspicious_referrer(line[16]):
-                possible_robots.add(remote_ip_str)
-            if suspicious_agent(line[17]):
-                possible_robots.add(remote_ip_str)
+        if suspicious_key(line[8]):
+            possible_robots.add(remote_ip_str)
+        if suspicious_referrer(line[16]):
+            possible_robots.add(remote_ip_str)
+        if suspicious_agent(line[17]):
+            possible_robots.add(remote_ip_str)
 
-for infile in log_files:
-    with open(os.path.join('logs', infile), 'r') as log:
-        log_reader = csv.reader(log, delimiter = ' ', quotechar = '"')
+for log in logs.db:
+    s = StringIO(log)
+    log_reader = csv.reader(s, delimiter = ' ', quotechar = '"')
 
-        first_in_file = True
-        for line in log_reader:
-            # Parse remote IP so it can be checked against prefixes
-            remote_ip_str = line[4]
-            remote_ip = ipre.search(remote_ip_str)
-            octets = ipre.search(remote_ip_str).groups()
+    first_in_file = True
+    for line in log_reader:
+        # Parse remote IP so it can be checked against prefixes
+        remote_ip_str = line[4]
+        remote_ip = ipre.search(remote_ip_str)
+        octets = ipre.search(remote_ip_str).groups()
 
-            # Check IP against manual prefixes to exclude from analysis
-            match = False
-            for prefix in ignore_prefix:
-                l = len(prefix)
-                if octets[0:l] == prefix:
-                    match = True
-            if match:
-                excluded.add(remote_ip_str)
-                break
+        # Check IP against manual prefixes to exclude from analysis
+        match = False
+        for prefix in ignore_prefix:
+            l = len(prefix)
+            if octets[0:l] == prefix:
+                match = True
+        if match:
+            excluded.add(remote_ip_str)
+            break
 
-            # Check IP against dynamic possible robots to exclude from analysis
-            if remote_ip_str in possible_robots:
-                break
+        # Check IP against dynamic possible robots to exclude from analysis
+        if remote_ip_str in possible_robots:
+            break
 
-            # Only analyze GET requests
-            if not line[7] == 'WEBSITE.GET.OBJECT':
-                continue
+        # Only analyze GET requests
+        if not line[7] == 'WEBSITE.GET.OBJECT':
+            continue
 
-            # Only analyze responses that actually served a real page
-            if not line[10][0] == '2':
-                continue
+        # Only analyze responses that actually served a real page
+        if not line[10][0] == '2':
+            continue
 
-            key = line[8]
-            if key == '-':
-                continue
-            res_hits[key] += 1
+        key = line[8]
+        if key == '-':
+            continue
+        res_hits[key] += 1
 
-            if first_in_file:
-                # As properties of the client, these only get recorded
-                # once per connection
+        if first_in_file:
+            # As properties of the client, these only get recorded
+            # once per connection
 
-                ip_hits[remote_ip_str] += 1
+            ip_hits[remote_ip_str] += 1
 
-                referrer = line[16]
-                if not referrer == '-':
-                    referrer_hits[referrer] += 1
-                agent = line[17]
-                if not agent == '-':
-                    agent_hits[agent.split(' ')[0]] += 1
+            referrer = line[16]
+            if not referrer == '-':
+                referrer_hits[referrer] += 1
+            agent = line[17]
+            if not agent == '-':
+                agent_hits[agent.split(' ')[0]] += 1
 
-                report = geoip.fetch(remote_ip_str)
-                if not report is None:
-                    if 'country_name' in report:
-                        country_name = report['country_name']
-                        if not country_name == '':
-                            country_hits[report['country_name']] += 1
-                    if 'region_name' in report:
-                        region_name = report['region_name']
-                        if not region_name == '':
-                            region_hits[report['region_name']] += 1
+            report = geoip.fetch(remote_ip_str)
+            if not report is None:
+                if 'country_name' in report:
+                    country_name = report['country_name']
+                    if not country_name == '':
+                        country_hits[report['country_name']] += 1
+                if 'region_name' in report:
+                    region_name = report['region_name']
+                    if not region_name == '':
+                        region_hits[report['region_name']] += 1
 
-                t = datetime.strptime(line[2][1:], '%d/%b/%Y:%H:%M:%S')
-                date_hits[t.strftime("%Y-%m-%d")] += 1
-                hour_hits[t.strftime("%H+0000'")] += 1
-                time_hits.append(t.hour + t.minute / 60 + t.second / 3600)
+            t = datetime.strptime(line[2][1:], '%d/%b/%Y:%H:%M:%S')
+            date_hits[t.strftime("%Y-%m-%d")] += 1
+            hour_hits[t.strftime("%H+0000'")] += 1
+            time_hits.append(t.hour + t.minute / 60 + t.second / 3600)
 
-                first_in_file = False
+            first_in_file = False
 
 # Cache GeoIP data
 geoip.dump()
